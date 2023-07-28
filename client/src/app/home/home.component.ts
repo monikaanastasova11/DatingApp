a4e6cdd99a037da00653b8879b742a311196031a
Monica Anastasova <venci7602@abv.bg>
2023-07-28 14:46:32 +0300

    End of section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
 
 
@@ -10,22 +10,14 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   registerMode = false;
   users:any;
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getUsers();
-  }
+    }
 registerToggle(){
   this.registerMode = !this.registerMode;
 }
 
-   getUsers(){
-    //this.http.get('https://localhost:5001/api/users').subscribe(users => this.users = users);
-    this.http.get('https://localhost:5001/api/users').subscribe({
-     next: response => this.users = response,
-      error:error =>console.log(error),
-      complete:() => console.log('The request has completed')
-  })}
 
   cancelRegisterMode(event : boolean){
     this.registerMode = event;
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -21,13 +21,9 @@ export class NavComponent implements OnInit {
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: _ => this.router.navigateByUrl('/members')
+      next: () => this.router.navigateByUrl('/members'),
       
-    }
-    
-    
-
-    )
+    });
   }
   logout(){
     this.accountService.logout();
