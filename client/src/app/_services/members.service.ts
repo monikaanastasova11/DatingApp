8776225e3d9c5f754585eb1bb0591548e339550a
Monica Anastasova <venci7602@abv.bg>
2023-08-18 17:06:25 +0300

    End of section 15

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -7,6 +7,7 @@ import { PaginatedResult } from '../_models/pagination';
 import { UserParams } from '../_models/userParams';
 import { AccountService } from './account.service';
 import { User } from '../_models/user';
+import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
 
 @Injectable({
   providedIn: 'root'
@@ -53,14 +54,14 @@ export class MembersService {
 
     if (response) return of (response);
 
-    let params = this.getPaginationHeaders(UserParams.pageNumber,UserParams.pageSize);
+    let params = getPaginationHeaders(UserParams.pageNumber,UserParams.pageSize);
 
     params = params.append('minAge', UserParams.minAge);
     params = params.append('maxAge', UserParams.maxAge);
     params = params.append('gender', UserParams.gender);
     params = params.append('orderBy', UserParams.orderBy);
 
-    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params).pipe(
+    return getPaginatedResult<Member[]>(this.baseUrl + 'users', params, this.http).pipe(
       map(response => {
         this.memberCache.set(Object.values(UserParams).join('-'), response);
         return response;
@@ -102,36 +103,13 @@ export class MembersService {
   }
 
   getLikes(predicate: string, pageNumber: number, pageSize: number){
-    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    let params = getPaginationHeaders(pageNumber, pageSize);
 
     params = params.append('predicate', predicate);
 
-    return this.getPaginatedResult<Member[]>(this.baseUrl + 'likes', params);
+    return getPaginatedResult<Member[]>(this.baseUrl + 'likes', params, this.http);
   }
 
-  private getPaginatedResult<T>(url: string, params: HttpParams) {
-    const paginatedresult: PaginatedResult<T> = new PaginatedResult<T>;
-    return this.http.get<T>(url, { observe: 'response', params }).pipe(
-      map(response => {
-        if (response.body) {
-          paginatedresult.result = response.body;
-        }
-        const pagination = response.headers.get('Pagination');
-        if (pagination) {
-          paginatedresult.pagination = JSON.parse(pagination);
-        }
-        return paginatedresult;
-      })
-
-    );
-  }
-
-  private getPaginationHeaders(pageNumber: number, pageSize: number) {
-      let params = new HttpParams();
-      params = params.append('pageNumber', pageNumber);
-      params = params.append('pageSize', pageSize);
-    
-    return params;
-  }
+  
 
 }
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
 import { Member } from 'src/app/_models/member';
+import { Message } from 'src/app/_models/message';
 import { MembersService } from 'src/app/_services/members.service';
+import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -10,15 +13,28 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent {
-  member: Member | undefined;
+  @ViewChild('memberTabs', {static:true}) memberTabs?: TabsetComponent;
+  member: Member = {} as Member;
   galleryOptions: NgxGalleryOptions [] = [];
   galleryImages: NgxGalleryImage[] = [];
+  activeTab?: TabDirective;
+  messages: Message[] = [];
 
   constructor(private memberService: MembersService,
-              private route: ActivatedRoute){}
+              private route: ActivatedRoute, private messageService: MessageService){}
 
   ngOnInit():void{
-    this.loadMember();
+  this.route.data.subscribe({
+    next: data => this.member = data['member']
+ })
+
+    this.route.queryParams.subscribe({
+      next: params => {
+        params['tab'] && this.selectTab(params['tab'])
+      }
+    })
+
+    this.getImages()
 
     this.galleryOptions = [
       {
@@ -34,6 +50,28 @@ export class MemberDetailComponent {
     
   }
 
+  onTabActivated(data: TabDirective){
+          this.activeTab = data;
+         if(this.activeTab.heading === 'Messages'){
+          this.loadMessages();
+         }
+    
+       }
+
+  selectTab(heading: string){
+    if(this.memberTabs){
+      this.memberTabs.tabs.find(x => x.heading === heading)!.active = true;
+
+    }
+  }
+    loadMessages() {
+      if(this.member)
+        this.messageService.getMessageThread(this.member.userName).subscribe({
+          next: messages => this.messages = messages
+      })
+    }
+
+
   getImages(){
     if(!this.member) return [];
     const imageUrls = [];
@@ -47,14 +85,21 @@ export class MemberDetailComponent {
     return imageUrls;
   }
 
-  loadMember(){
-    const username = this.route.snapshot.paramMap.get('username');
-    if(!username) return;
-    this.memberService.getMember(username).subscribe({
-      next: member => {
-        this.member = member;
-        this.galleryImages = this.getImages();
-      }
-    });
-  }
+  // loadMember(){
+  //   const username = this.route.snapshot.paramMap.get('username');
+  //   if(!username) return;
+  //   this.memberService.getMember(username).subscribe({
+  //     next: member => {
+  //       this.member = member;
+  //       this.galleryImages = this.getImages();
+  //     }
+  //   });
+  // }
 }
+
+
+
+
+
+
+
