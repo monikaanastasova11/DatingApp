b3f5071c90034153570c028f7a611e1540017e3c
Monica Anastasova <venci7602@abv.bg>
2023-08-25 22:45:24 +0300

    End of section 17

--- client/src/app/_members/member-messages/member-messages.component.ts
+++ client/src/app/_members/member-messages/member-messages.component.ts
@@ -11,10 +11,9 @@ import { MessageService } from 'src/app/_services/message.service';
 export class MemberMessagesComponent implements OnInit {
   @ViewChild('messageForm') messageForm?:NgForm
   @Input() username?: string;
-  @Input() messages: Message[] = [];
   messageContent = '';
  
-  constructor(private messageService: MessageService) { }
+  constructor(public messageService: MessageService) { }
  
   ngOnInit(): void {
     
@@ -22,17 +21,16 @@ export class MemberMessagesComponent implements OnInit {
 
   sendMessage(){
     if(!this.username) return;
-    this.messageService.sentMessage(this.username, this.messageContent).subscribe({
-      next: message => {
-        this.messages.push(message);
-        this.messageForm?.reset();
-      }
+    this.messageService.sentMessage(this.username, this.messageContent).then(() => {
+      this.messageForm?.reset();
     })
+      
+    }
   }
  
 
  
-}
+
 
 
 
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,27 +1,38 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
+import { take } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { Message } from 'src/app/_models/message';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 import { MessageService } from 'src/app/_services/message.service';
+import { PresenceService } from 'src/app/_services/presence.service';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css']
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit, OnDestroy {
   @ViewChild('memberTabs', {static:true}) memberTabs?: TabsetComponent;
   member: Member = {} as Member;
   galleryOptions: NgxGalleryOptions [] = [];
   galleryImages: NgxGalleryImage[] = [];
   activeTab?: TabDirective;
   messages: Message[] = [];
+  user?: User;
 
-  constructor(private memberService: MembersService,
-              private route: ActivatedRoute, private messageService: MessageService){}
+  constructor(private accountService: AccountService, private route: ActivatedRoute,
+     private messageService: MessageService, public presenceService: PresenceService){
+      this.accountService.currentUser$.pipe(take(1)).subscribe({
+        next: user => {
+          if (user) this.user = user;
+        }
+      })
+     }
 
   ngOnInit():void{
   this.route.data.subscribe({
@@ -50,10 +61,16 @@ export class MemberDetailComponent {
     
   }
 
+  ngOnDestroy(): void {
+    this.messageService.stopHubConnection();
+  }
+
   onTabActivated(data: TabDirective){
           this.activeTab = data;
-         if(this.activeTab.heading === 'Messages'){
-          this.loadMessages();
+         if(this.activeTab.heading === 'Messages' && this.user){
+          this.messageService.createHubConnection(this.user, this.member.userName)
+         }else {
+          this.messageService.stopHubConnection();
          }
     
        }
@@ -85,16 +102,7 @@ export class MemberDetailComponent {
     return imageUrls;
   }
 
-  // loadMember(){
-  //   const username = this.route.snapshot.paramMap.get('username');
-  //   if(!username) return;
-  //   this.memberService.getMember(username).subscribe({
-  //     next: member => {
-  //       this.member = member;
-  //       this.galleryImages = this.getImages();
-  //     }
-  //   });
-  // }
+  
 }
 
 
